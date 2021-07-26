0de79ce47d1311bff7f968273cb68e7e98ef8b06
Олег Терентьев <oleg.terentev123456789@outlook.com>
2021-07-26 19:12:55 +0300

    Add dbcontext. Create db connection string. Install nesessary packages.

--- HomeLibrary.WebAPI/Startup.cs
+++ HomeLibrary.WebAPI/Startup.cs
@@ -1,7 +1,9 @@
+using HomeLibrary.DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +34,9 @@ namespace HomeLibrary.WebAPI
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HomeLibrary", Version = "v1" });
             });
+
+            services.AddDbContext<HomeLibraryDbContext>(options =>
+                options.UseSqlServer(Configuration["ConnectionStrings:HomeLibraryDatabase"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
