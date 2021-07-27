874146f73a12782f84ff5c53fc5c43b7a3f7ec1b
Олег Терентьев <oleg.terentev123456789@outlook.com>
2021-07-27 15:32:16 +0300

    Create general service interface

---
+++ HomeLibrary.BLL/Interfaces/IService.cs
@@ -0,0 +1,18 @@
+﻿using HomeLibrary.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeLibrary.BLL.Interfaces
+{
+    public interface IService<T> where T: BaseEntity
+    {
+        Task<ICollection<T>> GetAllAsync();
+        Task<T> GetAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task Delete(int id);
+    }
+}
