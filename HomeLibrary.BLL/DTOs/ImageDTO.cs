c73fdd995deb1ac9a27ae80facd21af3829b2121
Олег Терентьев <oleg.terentev123456789@outlook.com>
2021-07-29 16:30:53 +0300

    Create image dto class

---
+++ HomeLibrary.BLL/DTOs/ImageDTO.cs
@@ -0,0 +1,13 @@
+﻿using HomeLibrary.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeLibrary.BLL.DTOs
+{
+    public class ImageDTO : BaseEntity
+    {
+        public string Url { get; set; }
+    }
+}
