1a92e322213d6079ca2549dfff2724dc5573e914
Олег Терентьев <oleg.terentev123456789@outlook.com>
2021-07-29 16:29:53 +0300

    Install AutoMapper. Create mapping profiles.

---
+++ HomeLibrary.BLL/Mapping/ImageProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using HomeLibrary.BLL.DTOs;
+using HomeLibrary.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeLibrary.BLL.Mapping
+{
+    public class ImageProfile : Profile
+    {
+        public ImageProfile()
+        {
+            CreateMap<Image, ImageDTO>();
+            CreateMap< ImageDTO, Image>();
+        }
+    }
+}
