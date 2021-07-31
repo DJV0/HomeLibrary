5c1e6e9c881fb1245443e6f31a7f6490a03d2570
Олег Терентьев <oleg.terentev123456789@outlook.com>
2021-07-31 19:37:18 +0300

    Create author controller. Implement Get, Post and Delete methods

---
+++ HomeLibrary.WebAPI/Controllers/AuthorsController.cs
@@ -0,0 +1,50 @@
+﻿using HomeLibrary.BLL.DTOs;
+using HomeLibrary.BLL.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeLibrary.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly IAuthorService _authorService;
+        public AuthorsController(IAuthorService authorService)
+        {
+            _authorService = authorService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ICollection<AuthorDTO>>> Get()
+        {
+            return Ok(await _authorService.GetAllAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AuthorDTO>> Get(int id)
+        {
+            return Ok(await _authorService.GetAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] AuthorDTO author)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            var newAuthor = await _authorService.AddAsync(author);
+            return CreatedAtAction(nameof(Get), new { id = newAuthor.Id }, newAuthor);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await _authorService.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
