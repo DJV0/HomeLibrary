2cde7dcc5f4a74b34919f707834bb8ecff1434a4
Олег Терентьев <oleg.terentev123456789@outlook.com>
2021-07-31 19:34:26 +0300

    Refactoring

--- HomeLibrary.BLL/Interfaces/IAuthorService.cs
+++ HomeLibrary.BLL/Interfaces/IAuthorService.cs
@@ -1,13 +1,13 @@
-﻿using HomeLibrary.DAL.Entities;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HomeLibrary.BLL.DTOs;
 
 namespace HomeLibrary.BLL.Interfaces
 {
-    public interface IAuthorService : IService<Author>
+    public interface IAuthorService : IService<AuthorDTO>
     {
     }
 }
