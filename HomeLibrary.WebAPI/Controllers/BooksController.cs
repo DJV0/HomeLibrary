49e498107b0a895cbd03bbf4ef29524f2b5d028c
Олег Терентьев <oleg.terentev123456789@outlook.com>
2021-08-01 17:49:39 +0300

    Create book controller. Implement CRUD methods

---
+++ HomeLibrary.WebAPI/Controllers/BooksController.cs
@@ -0,0 +1,60 @@
+﻿using HomeLibrary.BLL.DTOs;
+using HomeLibrary.BLL.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HomeLibrary.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookService _bookService;
+        public BooksController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ICollection<BookDTO>>> Get()
+        {
+            return Ok(await _bookService.GetAllAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BookDTO>> Get(int id)
+        {
+            return Ok(await _bookService.GetAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] BookDTO book)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            var newBook = await _bookService.AddAsync(book);
+            return CreatedAtAction(nameof(Get), new { id = newBook.Id }, newBook);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] BookDTO book)
+        {
+            if (id != book.Id) ModelState.AddModelError("Id", "Input Id doesn't match.");
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            await _bookService.UpdateAsync(book);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await _bookService.DeleteAsync(id);
+            return NoContent();
+        }
+
+    }
+}
