db4bae85b87950cc7eacdfc0743dba5605ee4c5e
Oleh Terentiev <dejavucool97gmail.com>
2021-12-23 18:25:43 +0200

    Create mapper configurator for BLL

---
+++ HomeLibrary.BLL/Infrastructure/MapperConfigurator.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using HomeLibrary.DAL.Entities;
+using HomeLibrary.Shared.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeLibrary.BLL.Infrastructure
+{
+    public class MapperConfigurator : Profile
+    {
+        public MapperConfigurator(IMapperConfigurationExpression config)
+        {
+            config.CreateMap<Author, AuthorDto>().ReverseMap();
+            config.CreateMap<Image, ImageDto>().ReverseMap();
+            config.CreateMap<Tag, TagDto>().ReverseMap();
+            config.CreateMap<Book, BookDto>().ReverseMap();
+        }
+    }
+}
