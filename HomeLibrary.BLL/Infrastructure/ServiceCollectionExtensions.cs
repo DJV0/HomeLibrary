56937d0713866cf3a2f03f952b359db91fa89cf2
Oleh Terentiev <dejavucool97gmail.com>
2021-12-23 18:25:18 +0200

    Create extension method for service collection to configure DI

---
+++ HomeLibrary.BLL/Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using HomeLibrary.BLL.Interfaces;
+using HomeLibrary.BLL.Services;
+using HomeLibrary.DAL.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeLibrary.BLL.Infrastructure
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection ConfigureBusinessLogicLayerDI(this IServiceCollection services, 
+                                                                                                IConfiguration configuration)
+        {
+            services.ConfigureDataAccessLayerDI(configuration);
+
+            services.AddScoped<IImageService, ImageService>();
+            services.AddScoped<IAuthorService, AuthorService>();
+            services.AddScoped<IBookService, BookService>();
+
+            var mapperConfiguration = new MapperConfiguration(config =>
+            {
+                config.AddProfile(new MapperConfigurator(config));
+            });
+            services.AddSingleton(mapperConfiguration.CreateMapper());
+
+            return services;
+        }
+    }
+}
