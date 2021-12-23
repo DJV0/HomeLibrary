b67d8dc179c0db0123817e80c2bc0a8f44ce96d1
Oleh Terentiev <dejavucool97gmail.com>
2021-12-23 18:16:03 +0200

    Create extension method for service collection to configure DbContext

---
+++ HomeLibrary.DAL/Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeLibrary.DAL.Infrastructure
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection ConfigureDataAccessLayerDI(this IServiceCollection services, 
+                                                                                        IConfiguration configuration)
+        {
+            services.AddDbContext<HomeLibraryDbContext>(opt => 
+                opt.UseSqlServer(configuration.GetConnectionString("HomeLibrary")));
+
+            return services;
+        }
+    }
+}
