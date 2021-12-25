a928b4f8bdd94aca42c0bf462ce7282ff44a6585
Oleh Terentiev <dejavucool97gmail.com>
2021-12-25 20:11:23 +0200

    Create Tag service

---
+++ HomeLibrary.BLL/Interfaces/ITagService.cs
@@ -0,0 +1,19 @@
+﻿using HomeLibrary.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeLibrary.BLL.Interfaces
+{
+    public interface ITagService
+    {
+        Task<ICollection<Tag>> GetAllAsync();
+        Task<Tag> GetByNameAsync(string name);
+        Task<Tag> AddAsync(Tag tag);
+        Task UpdateAsync(Tag entity);
+        Task DeleteAsync(string name);
+        Task DeleteAsync(Tag tag);
+    }
+}
---
+++ HomeLibrary.BLL/Services/TagService.cs
@@ -0,0 +1,56 @@
+﻿using HomeLibrary.BLL.Interfaces;
+using HomeLibrary.DAL;
+using HomeLibrary.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeLibrary.BLL.Services
+{
+    public class TagService : ITagService
+    {
+        private readonly HomeLibraryDbContext _dbContext;
+        public TagService(HomeLibraryDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        public async Task<Tag> AddAsync(Tag tag)
+        {
+            await _dbContext.Tags.AddAsync(tag);
+            await _dbContext.SaveChangesAsync();
+            return tag;
+        }
+
+        public async Task DeleteAsync(string name)
+        {
+            Tag tag = await GetByNameAsync(name);
+            if (tag != null) await DeleteAsync(tag);
+        }
+
+        public async Task DeleteAsync(Tag tag)
+        {
+            _dbContext.Tags.Remove(tag);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<ICollection<Tag>> GetAllAsync()
+        {
+            return await _dbContext.Tags.ToListAsync();
+        }
+
+        public async Task<Tag> GetByNameAsync(string name)
+        {
+            return await _dbContext.Tags.FindAsync(name);
+        }
+
+        public async Task UpdateAsync(Tag entity)
+        {
+            _dbContext.Tags.Update(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
