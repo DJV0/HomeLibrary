7d1ee59bc060f28ed021e5bc2e3ad93ef8bacb0b
Oleh Terentiev <dejavucool97gmail.com>
2022-01-17 19:59:05 +0200

    Create service to upload files to Azure

---
+++ HomeLibrary.Client/Infrastructure/UploadFileService.cs
@@ -0,0 +1,50 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeLibrary.Client.Infrastructure
+{
+    public class UploadFileService
+    {
+        private readonly string _connectionString;
+        private readonly string _blobContainerName = "images";
+
+        public UploadFileService(string connectionString/*, string containerName,*/)
+        {
+            _connectionString = connectionString;
+            //_blobContainerName = containerName;
+        }
+
+        public async Task<string> UploadAsync(IBrowserFile file)
+        {
+            if(file.Size > 0)
+            {
+                try
+                {
+                    var blobContainerClient = new BlobContainerClient(_connectionString, _blobContainerName);
+                    var createResponse = await blobContainerClient.CreateIfNotExistsAsync(PublicAccessType.Blob);
+
+                    BlobClient blobClient = blobContainerClient.GetBlobClient(file.Name);
+                    using (var fileStream = file.OpenReadStream())
+                    {
+                        await blobClient.UploadAsync(fileStream, new BlobHttpHeaders { ContentType = file.ContentType });
+                    }
+                    return blobClient.Uri.ToString();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                
+            }
+
+            return String.Empty;
+        }
+    }
+}
