868c43f0290fce4d2103d37c961067cec7143555
Oleh Terentiev <dejavucool97gmail.com>
2022-01-28 17:12:08 +0200

    Add number of pages to Book entity. Initialize collections

--- HomeLibrary.Shared/Dto/BookDto.cs
+++ HomeLibrary.Shared/Dto/BookDto.cs
@@ -15,8 +15,9 @@ namespace HomeLibrary.Shared.Dto
         public string Title { get; set; }
         public string ISBN { get; set; }
         public int PublishDate { get; set; }
-        public ICollection<AuthorDto> Authors { get; set; }
-        public ICollection<ImageDto> Images { get; set; }
-        public ICollection<TagDto> Tags { get; set; }
+        public int NumberOfPages { get; set; }
+        public ICollection<AuthorDto> Authors { get; set; } = new List<AuthorDto>();
+        public ICollection<ImageDto> Images { get; set; } = new List<ImageDto>();
+        public ICollection<TagDto> Tags { get; set; } = new List<TagDto>();
     }
 }
