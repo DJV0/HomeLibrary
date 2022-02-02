5fba699c6c8e962d1b63a4ce45eba6993bd756ad
Oleh Terentiev <dejavucool97gmail.com>
2022-02-02 15:09:25 +0200

    Add mapper setting to author name property

--- HomeLibrary.BLL/Infrastructure/MapperConfigurator.cs
+++ HomeLibrary.BLL/Infrastructure/MapperConfigurator.cs
@@ -14,8 +14,14 @@ namespace HomeLibrary.BLL.Infrastructure
         public MapperConfigurator(IMapperConfigurationExpression config)
         {
             config.CreateMap<Author, AuthorDto>()
-                .ForMember(authorDto=>authorDto.FullName, opt=>opt.MapFrom(author=>author.FirstName+" "+author.LastName))
-                .ReverseMap();
+                .ForMember(authorDto => authorDto.FullName, opt => opt
+                                                                .MapFrom(author => author.FirstName + " " + author.LastName));
+            config.CreateMap<AuthorDto, Author>()
+                .ForMember(author => author.FirstName, opt => opt
+                                            .MapFrom(authorDto => authorDto.FullName.Split(' ', StringSplitOptions.None)[0]))
+                .ForMember(author => author.LastName, opt => opt
+                                            .MapFrom(authorDto => authorDto.FullName.Split(' ', StringSplitOptions.None)[1]));
+
             config.CreateMap<Image, ImageDto>().ReverseMap();
             config.CreateMap<Tag, TagDto>().ReverseMap();
             config.CreateMap<Book, BookDto>().ReverseMap();
